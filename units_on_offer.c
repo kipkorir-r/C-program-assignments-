1e2964a8df9b6ac9d922e1fc789ef9c70206a12c
kipkorir-r <robinkipkorir562@gmail.com>
2025-10-01 18:46:13 +0300

    Add files via upload

---
+++ units_on_offer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>//standard input-output file
+ int main()//Program begins
+ {
+   //The function of \n is to display the text on a new line of its own
+   printf("Units Offered This Semister:\n");//The title
+   //printing the list of units in offer
+   printf("1.introducction to Programing\n");
+   printf("2.Data Structures \n");
+   printf("3.Computer Networks\n");
+   printf("4.Database Systems\n");
+   printf("5.Software Engineering\n");
+ return 0;//Execution is done
+}
