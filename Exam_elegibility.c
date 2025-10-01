a464e0ae7f1a673335ce1b390813c9dbd30efa93
kipkorir-r <robinkipkorir562@gmail.com>
2025-10-01 18:25:15 +0300

    Add files via upload

---
+++ Exam_elegibility.c
@@ -0,0 +1,19 @@
+#include <stdio.h>//starndard input output
+
+int main()
+ {
+    float attendance, marks;
+    // user to enter ther attendnce percentage and marks
+    printf("Enter attendance percentage: ");
+    scanf("%f", &attendance);
+    printf("Enter average marks: ");
+    scanf("%f", &marks);
+    //checking eligibility
+    if (attendance >= 75 && marks >= 40) 
+        printf("Eligible for final exams.\n");
+    else 
+        printf("Not eligible.\n");
+    
+
+    return 0;//program ends
+}
