61a94f70cee6bc691801114a10295703a3d350ad
kipkorir-r <robinkipkorir562@gmail.com>
2025-10-08 23:07:53 +0300

    Update Password_checker.c

--- Password_checker.c
+++ Password_checker.c
@@ -2,23 +2,26 @@
 PA106/G/28705/25
 Robin kipkorir Mutai*/
 
-
 #include <stdio.h>//input output
 #include <string.h>//string library having string comparison
 
-int main() {
+int main()
+{
     char password[20];//seetting the variable passwrd to character
 
-    do {//start of do while loop
+    do //start of do while loop
+    {
         printf("Enter password: ");//user enters password
         scanf("%s", &password);  // entered password is stored
 
-        if (strcmp(password, "1234") != 0) {/*the strcmp() compares the password enter with the 
+        if (strcmp(password, "1234") != 0) /*the strcmp() compares the password enter with the 
 correct password and returns a value 0 if they are the same*/
-            printf("Wrong password, try again.\n");
-        }//displayed when the password doesnt match 
+        {
+            printf("Wrong password, try again.\n");//displayed when the password doesnt match 
+        }
     } while (strcmp(password, "1234") != 0);//the loop is repeated when the password is incorrect
 
     printf("Access Granted\n");//printed when password is correct
     return 0;//program ends
+
 }
