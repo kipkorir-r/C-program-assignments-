3a54eeaaa04c962bc083f118ccb2ce28f74e3b21
kipkorir-r <robinkipkorir562@gmail.com>
2025-11-06 11:45:30 +0300

    Add files via upload

---
+++ LibraryBooks.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    FILE *file;
+    char title[100];
+
+    file = fopen("borrowed_books.txt", "a"); // Open in append mode
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    
+    while (1) {
+        printf("Book Title: ");
+        fgets(title, sizeof(title),stdin );
+
+        fprintf(file, "%s", title); // Save title in file
+        printf("Title stored successfully!\n");
+    }
+
+    fclose(file);
+    
+    return 0;
+}
